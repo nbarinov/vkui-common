5a07f77bb5b25afe150e5e82076f00d00e2bac36
Nikita Barinov <nikitabarinov2301@gmail.com>
2021-05-10 21:10:12 +0400

    add appHtml path

--- config/paths.js
+++ config/paths.js
@@ -31,6 +31,7 @@ module.exports = {
   appPath: resolveApp('.'),
   appBuild: resolveApp('build'),
   appPublic: resolveApp('public'),
+  appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveModule(resolveApp, 'src/index', moduleFileExtensions),
   appPackageJson: resolveApp('package.json'),
   appSrc: resolveApp('src'),
